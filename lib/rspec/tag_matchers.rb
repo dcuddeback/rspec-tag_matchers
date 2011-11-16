5a0f27367711cdf3055b03685823f5e17405af5e
David Cuddeback <david.cuddeback@gmail.com>
2011-11-16 14:51:39 -0800

    fix for uninitialized constant when RSpec is not loaded

--- lib/rspec/tag_matchers.rb
+++ lib/rspec/tag_matchers.rb
@@ -1,5 +1,10 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', 'core_ext'))
 
+module RSpec
+  module TagMatchers
+  end
+end
+
 require 'rspec/tag_matchers/has_tag'
 require 'rspec/tag_matchers/has_form'
 require 'rspec/tag_matchers/has_input'
